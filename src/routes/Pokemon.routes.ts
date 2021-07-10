92fdb26bbbd7ff8df17ff35d36039dbf7ec03000
Guilherme Augusto Vieira <guilhermeavdino@gmail.com>
2021-07-10 01:48:19 -0300

    API response update

--- src/routes/Pokemon.routes.ts
+++ src/routes/Pokemon.routes.ts
@@ -8,7 +8,7 @@ const controladorPokemon = new PokemonController();
 
 rotasPokemon.post("/:id", celebrate({
   [Segments.PARAMS]: Joi.object().keys({
-    id: Joi.number().required().min(1).max(898),
+    id: Joi.number().required().min(1),
   }),
 }), controladorPokemon.ler);
 
--- src/useCases/pokemon/controller/Pokemon.controller.ts
+++ src/useCases/pokemon/controller/Pokemon.controller.ts
@@ -17,46 +17,41 @@ export class PokemonController {
     const id = +requisicao.params.id;
     let dadosPokemon: Pokemons;
     let responseMessage;
-
-    const _pokeApi = container.resolve(PokeApiUseCase);
-    const dadosPokeApi = await _pokeApi.execute(id);
-
-    if(dadosPokeApi !== null) {      
-      try {
-        // Recebe os dados do pokemon
-        const _pokemon = container.resolve(PokemonsUseCase);
-        dadosPokemon = await _pokemon.execute(dadosPokeApi);
-
-        if(dadosPokemon !== undefined){
-          const _abilities = container.resolve(AbilitiesUseCase);
-          await _abilities.execute(dadosPokeApi, dadosPokemon);
-
-          const _gameIndices = container.resolve(GameIndicesUseCase);
-          await _gameIndices.execute(dadosPokeApi, dadosPokemon);
-                    
-          const _heldItems = container.resolve(HeldItemsUseCase);
-          await _heldItems.execute(dadosPokeApi, dadosPokemon);
-
-          const _moves = container.resolve(MovesUseCase);
-          await _moves.execute(dadosPokeApi, dadosPokemon);
+    
+    try {
+      const _pokeApi = container.resolve(PokeApiUseCase);
+      const dadosPokeApi = await _pokeApi.execute(id);
           
-          const _sprites = container.resolve(SpritesUseCase);
-          await _sprites.execute(dadosPokeApi, dadosPokemon);
-          
-          const _stats = container.resolve(StatsUseCase);
-          await _stats.execute(dadosPokeApi, dadosPokemon);
-          
-          const _pokemonTypes = container.resolve(PokemonTypesUseCase);
-          await _pokemonTypes.execute(dadosPokeApi, dadosPokemon);
+      // Recebe os dados do pokemon
+      const _pokemon = container.resolve(PokemonsUseCase);
+      dadosPokemon = await _pokemon.execute(dadosPokeApi);
 
-          responseMessage = `Pokemon ${dadosPokemon.id}: ${dadosPokemon.name} incluído`;
-        }
-      } catch (error) {
-        responseMessage = `Erro ao inserir o pokemon ${dadosPokemon.id}: ${dadosPokemon.name}`
-        throw error;
-      }
-    }else{
-      responseMessage = `Pokemon ${id} não encontrado`;
+      const _abilities = container.resolve(AbilitiesUseCase);
+      await _abilities.execute(dadosPokeApi, dadosPokemon);
+
+      const _gameIndices = container.resolve(GameIndicesUseCase);
+      await _gameIndices.execute(dadosPokeApi, dadosPokemon);
+                
+      const _heldItems = container.resolve(HeldItemsUseCase);
+      await _heldItems.execute(dadosPokeApi, dadosPokemon);
+
+      const _moves = container.resolve(MovesUseCase);
+      await _moves.execute(dadosPokeApi, dadosPokemon);
+      
+      const _sprites = container.resolve(SpritesUseCase);
+      await _sprites.execute(dadosPokeApi, dadosPokemon);
+      
+      const _stats = container.resolve(StatsUseCase);
+      await _stats.execute(dadosPokeApi, dadosPokemon);
+      
+      const _pokemonTypes = container.resolve(PokemonTypesUseCase);
+      await _pokemonTypes.execute(dadosPokeApi, dadosPokemon);
+
+      responseMessage = `Pokemon ${dadosPokemon.id}: ${dadosPokemon.name} incluído`;
+    }catch (error) {
+      if(error.response.status === 404) responseMessage = `Pokemon ${id} não cadastrado na PokeAPI`;
+      else responseMessage = `Erro ao inserir o pokemon ${id}`;
+      console.log(error);
     }
 
     return resposta.json(responseMessage);
